8dedac4e567c8a5976ecc7e0c07fcff79d706312
enginewang <engine74396@gmail.com>
2023-06-03 05:01:34 +0800

    first commit

---
+++ src/main/java/io/kubesphere/devops/HelloWorldController.java
@@ -0,0 +1,24 @@
+package io.kubesphere.devops;
+
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ */
+@RestController
+@EnableAutoConfiguration
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String sayHello() {
+        return "[v1.0] SpringBoot Deployed on Kubernetes with Jenkins DevOps.";
+    }
+
+    @RequestMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
